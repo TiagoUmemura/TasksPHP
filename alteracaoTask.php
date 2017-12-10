7a63187e392e7dc8087965156b3da9265a82aadf
Tiago Umemura <umemuratiago@gmail.com>
2017-12-10 00:39:24 -0200

    ALteracao de task OK

--- alteracaoTask.php
+++ alteracaoTask.php
@@ -25,5 +25,24 @@
 
 	if($arquivo != ''){
 		echo "arquivo descricao preenchido"."<br>";
+
+		//procurar registro do arquivo com o codigo no POST
+		$executa = $pdo->query("SELECT * FROM tasks WHERE Codigo = '$codigo'");
+		foreach($executa as $resultado){
+			echo "Arquivo: ".$resultado['Arquivo'].";"."<br>";
+			unlink($resultado['Arquivo']);
+		}
+
+		//update do caminho para o caminho do novo arquivo
+		$pdo->query("UPDATE tasks SET Arquivo = '$target_file' WHERE Codigo = '$codigo'");
+
+		//Upload novo arquivo
+		if (move_uploaded_file($_FILES['arquivo_alterar']['tmp_name'], $target_file)) {
+	        echo "The file ". basename( $_FILES["arquivo_alterar"]["name"]). " has been uploaded.";
+	    } else {
+	        echo "Upload failed with error code " . $_FILES['arquivo_alterar']['error'];
+	    }
 	}
+
+	header("Location: taskMain.php");
 ?>
