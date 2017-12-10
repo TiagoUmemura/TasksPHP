73d25f21c7ef95667c05fdc152673b8b413b298d
Tiago Umemura <umemuratiago@gmail.com>
2017-12-10 10:36:49 -0200

    coluna arquivo da lista com href para o arquivo na pasta uploads

--- listarTasks.php
+++ listarTasks.php
@@ -14,13 +14,6 @@
     <!-- Bootstrap Core CSS -->
     <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 </head>
 
 <body id="page-top" class="index">
@@ -48,11 +41,16 @@
 				echo "<td>".$resultado['Codigo']."</td>";
 				echo "<td>".$resultado['Nome']."</td>";
 				echo "<td>".$resultado['Descricao']."</td>";
-				echo "<td>".$resultado['Arquivo']."</td>";
+				echo "<td><a href=".$resultado['Arquivo'].">".$resultado['Arquivo']."</a></td>";
 				echo "</tr>";
 			}
 		?>
 	</table> 
+
+	<!-- Botao voltar-->
+	<form action="taskMain.php">
+    	<input type="submit" value="Voltar" />
+	</form>
 
 	</div>
 
